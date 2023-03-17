62394b6c7f4c1a796265d6d4dd14dd04731925ca
Miguel Teixeira <mteixeira88013@gmail.com>
2023-03-17 11:05:06 +0000

    removed unnecessary code

--- Shared/Extensions/DateExtension.swift
+++ Shared/Extensions/DateExtension.swift
@@ -20,17 +20,6 @@ extension Date {
     }
 }
 
-// Get date components
-extension Date {
-    
-    func get(_ components: Calendar.Component..., calendar: Calendar = Calendar.current) -> DateComponents {
-        return calendar.dateComponents(Set(components), from: self)
-    }
-
-    func get(_ component: Calendar.Component, calendar: Calendar = Calendar.current) -> Int {
-        return calendar.component(component, from: self)
-    }
-}
 
 extension DateFormatter {
     
