f0d24e3524f1b8d87fea7b6d80cad37bcb81913d
Miguel Teixeira <mteixeira88013@gmail.com>
2023-03-17 10:40:26 +0000

    removed unnecessary fonts

--- Shared/Styles/Fonts.swift
+++ Shared/Styles/Fonts.swift
@@ -2,23 +2,7 @@ import SwiftUI
 
 extension Font {
 
-    static let subheadlineFont: Self = .subheadline
-    static let titleFontBold: Self = .system(.title2, weight: .bold)
-    static let driverPositionFont: Self = .system(.title3, weight: .bold)
-    static let driverTickerFont: Self = .system(.headline, weight: .bold)
-
     static let newsCarouselPositionFont: Self = .system(size: 45, weight: .bold)
-    static let newsCarouselDriverNameFont: Self = .headline
-    static let newsCarouselConstrutorNameFont: Self = .subheadline.italic()
-
-    static let eventTitleFont: Self = .system(.title3, weight: .bold)
-    static let eventRoundNumberFont: Self = .body.italic()
-    static let liveSessionTitleFont: Self = .body.bold()
-    static let chevronRightFont: Self = .body
-    static let liveTextFont: Self = .body.bold()
-    
-    
-    
     
     enum Title {
         static let regular: Font = .system(size: 32)
