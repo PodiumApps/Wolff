307509bdadeeef1d883aea79c162d33ef69ba6ad
Tomás Mamede <tomas.sant.mamede@icloud.com>
2023-03-24 00:35:14 +0000

    Changes to liveView

--- watch/Slipstream_watch/Views/EventCard/Live/LiveEventCardView.swift
+++ watch/Slipstream_watch/Views/EventCard/Live/LiveEventCardView.swift
@@ -13,24 +13,36 @@ struct LiveEventCardView<ViewModel: LiveEventCardViewModelRepresentable>: View {
         Button(action: {
 
         }) {
-
-            VStack(alignment: .leading, spacing: .Spacing.default2) {
-                VStack(alignment: .leading) {
-                    Text(viewModel.title)
-                        .font(.Body.semibold)
-                    HStack(spacing: .Spacing.default3) {
-                        HStack(spacing: .Spacing.default) {
-                            Text("🇧🇪")
-                            Text(viewModel.country)
-                                .font(.system(size: 12, weight: .medium))
+            HStack {
+                VStack(alignment: .leading, spacing: .Spacing.default2) {
+                    VStack(alignment: .leading) {
+                        Text(viewModel.title)
+                            .font(.Body.semibold)
+                        HStack(spacing: .Spacing.default3) {
+                            HStack(spacing: .Spacing.default) {
+                                Text("🇧🇪")
+                                Text(viewModel.country)
+                                    .font(.system(size: 12, weight: .medium))
+                            }
+                            Text("Round \(viewModel.round.description)")
+                                .font(.system(size: 12, weight: .regular))
+                                .foregroundColor(Color.red)
                         }
-                        Text("Round \(viewModel.round.description)")
-                            .font(.system(size: 12, weight: .regular))
+                    }
+
+                    VStack(alignment: .leading, spacing: 1) {
+                        Text(viewModel.sessionName)
+                            .font(.system(size: 12, weight: .bold))
+                        Text("Happening Now".uppercased())
+                            .font(.system(size: 11, weight: .medium))
+                            .foregroundColor(.red)
                     }
                 }
+
+                Spacer()
             }
         }
-        .background(Color.red.opacity(0.3))
+        .background(Color.red.opacity(0.27))
         .buttonBorderShape(
             .roundedRectangle(radius: 10)
         )
