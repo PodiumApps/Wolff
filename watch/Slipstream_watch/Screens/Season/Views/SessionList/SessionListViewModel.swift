c416ee76059991ef1537f9f0e613fb9c9bbc04bb
Tomás Mamede <tomas.sant.mamede@icloud.com>
2023-04-10 10:23:10 +0100

    Improvements

--- watch/Slipstream_watch/Screens/Season/Views/SessionList/SessionListViewModel.swift
+++ watch/Slipstream_watch/Screens/Season/Views/SessionList/SessionListViewModel.swift
@@ -14,6 +14,9 @@ final class SessionListViewModel: SessionListViewModelRepresentable {
 
     private let eventService: EventServiceRepresentable
     private let driverAndConstructorService: DriverAndConstructorServiceRepresentable
+    private let liveEventService: LiveSessionServiceRepresentable
+
+    private var liveEventTimer: Timer? = nil
 
     @Published var state: State
     @Published private var sessionCells: [Cell]
@@ -21,7 +24,8 @@ final class SessionListViewModel: SessionListViewModelRepresentable {
     init(
         event: Event,
         eventService: EventServiceRepresentable,
-        driverAndConstructorService: DriverAndConstructorServiceRepresentable
+        driverAndConstructorService: DriverAndConstructorServiceRepresentable,
+        liveEventService: LiveSessionServiceRepresentable
     ) {
 
         self.event = event
@@ -30,6 +34,7 @@ final class SessionListViewModel: SessionListViewModelRepresentable {
 
         self.eventService = eventService
         self.driverAndConstructorService = driverAndConstructorService
+        self.liveEventService = liveEventService
 
         self.state = .loading
         self.sessionCells = []
@@ -63,6 +68,75 @@ final class SessionListViewModel: SessionListViewModelRepresentable {
                 }
             }
             .assign(to: &$state)
+
+        liveEventService.statePublisher
+            .receive(on: DispatchQueue.main)
+            .compactMap { [weak self] liveEventService -> SessionListViewModel.State? in
+
+                guard
+                    let self,
+                    case .results(var cells) = self.state,
+                    let index = cells.firstIndex(where: { $0.id == .live }),
+                    case .live(let timeInterval, _) = event.status
+                else {
+                    return nil
+                }
+
+                switch liveEventService {
+                case .refreshed(let positions):
+
+                    guard !positions.isEmpty else { return nil }
+
+                    let podium: [Driver.ID] = positions[0 ..< 3].compactMap { [weak self] position in
+
+                        guard let self else { return nil }
+
+                        guard let driver = self.drivers.first(where: { $0.id == position.id }) else { return nil }
+                        return driver.id
+                    }
+                    sessionCells[index] = .live(
+                        buildLiveSessionCellViewModel(
+                            sessionName: event.sessions[index].name,
+                            podium: podium,
+                            sessionDate: event.sessions[index].date,
+                            timeInterval: timeInterval
+                        )
+                    )
+
+                    return .results(sessionCells)
+
+                case .refreshing, .error:
+
+                    sessionCells[index] = .live(
+                        buildLiveSessionCellViewModel(
+                            sessionName: event.sessions[index].name,
+                            sessionDate: event.sessions[index].date,
+                            timeInterval: timeInterval
+                        )
+                    )
+
+                    return .results(sessionCells)
+                }
+            }
+            .assign(to: &$state)
+    }
+
+    private func updateLiveEventTimer(timeInterval: TimeInterval, triggerInterval: Double) {
+
+        liveEventTimer?.invalidate()
+
+        liveEventTimer =
+        Timer.scheduledTimer(withTimeInterval: triggerInterval, repeats: true) { [weak self, timeInterval] _ in
+
+                guard let self else { return }
+
+                if timeInterval < .minuteInterval {
+
+                    self.liveEventService.action.send(.updatePositions)
+                }
+
+                self.state = self.buildSessionRows()
+        }
     }
 
     private func buildSessionRows() -> State {
@@ -74,6 +148,9 @@ final class SessionListViewModel: SessionListViewModelRepresentable {
             if case .live(let timeInterval, _) = event.status {
                 if session.winners.isEmpty && !liveEventCellBuilt {
                     liveEventCellBuilt = true
+
+                    updateLiveEventTimer(timeInterval: timeInterval, triggerInterval: 10)
+
                     return .live(
                         buildLiveSessionCellViewModel(
                             sessionName: session.name,
@@ -102,7 +179,10 @@ final class SessionListViewModel: SessionListViewModelRepresentable {
         return .results(sessionCells)
     }
 
-    private func buildUpcomingSessionCellViewModel(sessionName: Session.Name, date: Date) -> UpcomingSessionCellViewModel {
+    private func buildUpcomingSessionCellViewModel(
+        sessionName: Session.Name,
+        date: Date)
+    -> UpcomingSessionCellViewModel {
 
         return UpcomingSessionCellViewModel(
             sessionName: sessionName.label,
@@ -229,7 +309,8 @@ extension SessionListViewModel {
         .init(
             event: event,
             eventService: ServiceLocator.shared.eventService,
-            driverAndConstructorService: ServiceLocator.shared.driverAndConstructorService
+            driverAndConstructorService: ServiceLocator.shared.driverAndConstructorService,
+            liveEventService: ServiceLocator.shared.liveSessionService
         )
     }
 }
