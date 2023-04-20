da33171d2d5d9f71b59db589aebc1f55cac486f0
Tomás Mamede <tomas.sant.mamede@icloud.com>
2023-04-20 21:50:52 +0100

    Keeping going!

--- watch/Slipstream_watch/Screens/Season/Views/SessionStandingsList/SessionStandingsViewModel.swift
+++ watch/Slipstream_watch/Screens/Season/Views/SessionStandingsList/SessionStandingsViewModel.swift
@@ -8,7 +8,11 @@ protocol SessionStandingsListViewModelRepresentable {
 final class SessionStandingsListViewModel: SessionStandingsListViewModelRepresentable {
 
     private let sessionID: Session.ID
+    private var drivers: [Driver]
+    private var constructors: [Constructor]
+
     private let liveEventService: LiveSessionServiceRepresentable
+    private let driverAndConstructorService: DriverAndConstructorServiceRepresentable
     private let networkManager: NetworkManagerRepresentable
 
     @Published var state: State
@@ -17,17 +21,18 @@ final class SessionStandingsListViewModel: SessionStandingsListViewModelRepresen
     init(
         sessionID: Session.ID,
         liveEventService: LiveSessionServiceRepresentable,
+        driverAndConstructorService: DriverAndConstructorServiceRepresentable,
         networkManager: NetworkManagerRepresentable
     ) {
 
         self.sessionID = sessionID
         self.liveEventService = liveEventService
+        self.driverAndConstructorService = driverAndConstructorService
         self.networkManager = networkManager
 
         self.state = .loading
         self.cells = []
 
-        self.state = self.buildStandingsCells()
         self.setUpServices()
     }
 
@@ -37,26 +42,74 @@ final class SessionStandingsListViewModel: SessionStandingsListViewModelRepresen
             .receive(on: DispatchQueue.main)
             .compactMap { [weak self] liveEventService -> SessionStandingsListViewModel.State? in
 
+                guard let self else { return nil}
+
                 switch liveEventService {
                 case .refreshed(let liveSession):
 
-                    guard !liveSession.standings.isEmpty else { return nil }
+                    guard !liveSession.standings.isEmpty else {
 
-                    return .results([])
+                        Task { await self.buildFinishedStandingsCells() }
+                        return nil
+                    }
 
-                case .refreshing, .error:
+                    return buildStandingsCells(standings: liveSession.standings)
 
-                    return .results([])
+                case .refreshing:
+                    return .loading
+                case .error(let error):
+                    return .error(error.localizedDescription)
+                }
+            }
+            .assign(to: &$state)
+
+        driverAndConstructorService.statePublisher
+            .receive(on: DispatchQueue.main)
+            .compactMap { [weak self] driverAndConstructorService -> SessionStandingsListViewModel.State? in
+
+                guard let self else { return nil }
+
+                switch driverAndConstructorService {
+                case .refreshed(let drivers, let constructors):
+
+                    self.drivers = drivers
+                    self.constructors = constructors
+                case .refreshing:
+                    return .loading
+                case .error(let error):
+                    return .error(error.localizedDescription)
                 }
             }
             .assign(to: &$state)
     }
 
-    private func buildStandingsCells() -> State {
+    private func buildStandingsCells(standings: [LiveSession.Position]) -> State {
 
+        let cells: [DriverStandingCellViewModel] = standings.compactMap { position in
 
+            let driver = self.drivers.first(where: { $0.id == position.id })
+
+            guard let driver else {   }
+
+            return .init(
+                firstName: driver.fullName,
+                lastName: driver.lastName,
+                team: driver.constructorId,
+                position: position.position,
+                time: position.time,
+                carNumber: driver.carNumber,
+                tyre: position.tyre
+            )
+        }
 
         return .results([])
+    }
+
+    private func buildFinishedStandingsCells() async {
+
+//        let sessionDetails = try? await networkManager.load(SessionDetail.getSession(id: sessionID.string))
+
+        self.state = .results([])
     }
 }
 
@@ -99,6 +152,7 @@ extension SessionStandingsListViewModel {
         .init(
             sessionID: sessionID,
             liveEventService: ServiceLocator.shared.liveSessionService,
+            driverAndConstructorService: ServiceLocator.shared.driverAndConstructorService,
             networkManager: NetworkManager.shared
         )
     }
