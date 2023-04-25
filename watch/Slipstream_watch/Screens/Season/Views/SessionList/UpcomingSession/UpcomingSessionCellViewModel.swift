ed9b4679a6a7fb57b005ab9bbed9860647ff2573
Tomás Mamede <tomas.sant.mamede@icloud.com>
2023-04-25 10:18:18 +0100

    Update UpcomingSessionCellViewModel

--- watch/Slipstream_watch/Screens/Season/Views/SessionList/UpcomingSession/UpcomingSessionCellViewModel.swift
+++ watch/Slipstream_watch/Screens/Season/Views/SessionList/UpcomingSession/UpcomingSessionCellViewModel.swift
@@ -3,30 +3,27 @@ import Foundation
 protocol UpcomingSessionCellViewModelRepresentable {
 
     var sessionName: String { get }
-    var date: Date { get }
     var formattedDate: String { get }
 }
 
 final class UpcomingSessionCellViewModel: UpcomingSessionCellViewModelRepresentable {
 
     var sessionName: String
-    var date: Date
     var formattedDate: String
 
     init(sessionName: String, date: Date) {
 
         self.sessionName = sessionName
-        self.date = date
 
-        if self.date.isSessionToday {
+        if date.isSessionToday {
 
-            self.formattedDate = Localization.SessionTime.today(DateFormatter.timeForSession.string(from: self.date))
-        } else if self.date.isSessionTomorrow {
+            self.formattedDate = Localization.SessionTime.today(DateFormatter.timeForSession.string(from: date))
+        } else if date.isSessionTomorrow {
 
-            self.formattedDate = Localization.SessionTime.tomorrow(DateFormatter.timeForSession.string(from: self.date))
+            self.formattedDate = Localization.SessionTime.tomorrow(DateFormatter.timeForSession.string(from: date))
         } else {
-            
-            self.formattedDate = DateFormatter.session.string(from: self.date)
+
+            self.formattedDate = DateFormatter.session.string(from: date)
         }
     }
 }
