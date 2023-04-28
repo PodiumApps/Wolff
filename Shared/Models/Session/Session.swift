d15f39090abccd63ee541b97a91044492174e896
Tomás Mamede <tomas.sant.mamede@icloud.com>
2023-04-28 16:59:33 +0100

    Correction

--- Shared/Models/Session/Session.swift
+++ Shared/Models/Session/Session.swift
@@ -21,6 +21,7 @@ extension Session {
         case qualifying = "Qualifying"
         case sprintShootout = "Sprint Shootout"
         case sprint = "Sprint"
+        case sprintShootout = "Sprint Shootout"
         case race = "Race"
 
         var label: String {
@@ -32,6 +33,7 @@ extension Session {
             case .qualifying: return "Qualifying"
             case .sprintShootout: return "Sprint Shootout"
             case .sprint: return "Sprint Race"
+            case .sprintShootout: return "Sprint Shootout"
             case .race: return "Race"
             }
         }
