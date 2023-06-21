a9e885615c0e466203ff46b7e4e768f533022833
Tomás Mamede <tomas.sant.mamede@icloud.com>
2023-06-21 17:39:41 +0100

    update

--- watch/Slipstream_watch/Notifications/LatestNews.swift
+++ watch/Slipstream_watch/Notifications/LatestNews.swift
@@ -3,24 +3,37 @@ import UserNotifications
 
 class LatestNewsNotificationController: WKUserNotificationHostingController<LatestNewsNotificationView> {
 
-    var content: UNNotificationContent!
-    var date: Date!
+    var newsTitle: String!
 
     override var body: LatestNewsNotificationView {
-        LatestNewsNotificationView()
+        LatestNewsNotificationView(title: newsTitle)
     }
 
     override func didReceive(_ notification: UNNotification) {
 
-        self.content = notification.request.content
-        self.date = notification.date
+        self.newsTitle = notification.request.content.body
     }
 }
 
 struct LatestNewsNotificationView: View {
 
+    @AppStorage(UserDefaultsKeys.isActiveLatestNewsNotification.rawValue)
+    var isActiveLatestNewsNotification: Bool = false
+
+    var title: String
+
     var body: some View {
-        Text("This is a Notification")
+        HStack {
+            VStack(alignment: .leading, spacing: .Spacing.default) {
+                Text("Slipstream News")
+                    .font(.Body.semibold)
+                Text(title)
+                    .font(.Body.regular)
+            }
+            .frame(width: .infinity)
+
+            Spacer()
+        }
     }
 }
 
