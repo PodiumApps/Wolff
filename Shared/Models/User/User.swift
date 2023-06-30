b6eb7c1e85ed9ef3bb0ae6d77df7f41d1dfb5721
Miguel Teixeira <mteixeira88013@gmail.com>
2023-06-30 11:21:30 +0100

    User device token update (#16)

--- Shared/Models/User/User.swift
+++ Shared/Models/User/User.swift
@@ -1,4 +1,5 @@
 import Foundation
+import OSLog
 
 struct User: Codable {
     
@@ -13,21 +14,47 @@ extension User {
         let id: User.ID
         let country: String
         let isPremium: Bool
+        let deviceToken: String?
     }
 }
 
 extension User {
     
-    static func createOrUpdate(isPremium: Bool = false) -> Resource<User> {
-        
-        @UserDefaultsWrapper(key: .user) var persistedUserId: String?
+    static func createOrUpdate(isPremium: Bool = false) -> Resource<Self> {
         
         guard let url = URL(string: "\(Global.url)/v1/user") else { fatalError("URL not found.") }
         
+        guard let persistedUserId  = UserDefaults.standard.string(forKey: UserDefaultsKeys.user.rawValue) else {
+            return .init(url: url, method: .get(accessToken: nil))
+        }
+        
+        Logger.userService.log("Posting user \(persistedUserId)")
+        
         let user: User.Post = .init(
-            id: .init(persistedUserId ?? UUID().uuidString),
+            id: .init(persistedUserId),
             country: Locale.current.language.region?.identifier ?? "No country",
-            isPremium: isPremium
+            isPremium: isPremium,
+            deviceToken: nil
+        )
+        
+        return Resource(url: url, method: .post(body: user, accessToken: Date().tokenString))
+    }
+    
+    static func update(deviceToken: String) -> Resource<Self> {
+        
+        guard let url = URL(string: "\(Global.url)/v1/user") else { fatalError("URL not found.") }
+        
+        guard let persistedUserId  = UserDefaults.standard.string(forKey: UserDefaultsKeys.user.rawValue) else {
+            return .init(url: url, method: .get(accessToken: nil))
+        }
+        
+        Logger.userService.log("Updating Device token \(deviceToken) for user \(persistedUserId)")
+        
+        let user: User.Post = .init(
+            id: .init(persistedUserId),
+            country: Locale.current.language.region?.identifier ?? "No country",
+            isPremium: false, // ⚒️ To not have to create a new model we will send this despite not being used by the BE
+            deviceToken: deviceToken
         )
         
         return Resource(url: url, method: .post(body: user, accessToken: Date().tokenString))
